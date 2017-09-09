0bbf96638d6c89bd5b04f5887e0bbbadf144cb47
bassihani <bassihani@gmail.com>
2017-09-09 01:25:20 -0400

    Removed unwanted Printf

--- atoi.c
+++ atoi.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 int atoi(const char*);
+
 int main(void){
 	char str[5];
 	strcpy(str,"12345576");
 	printf("%d\n",atoi(str));
-	//printf("%d\n",atoi(str));
 	return 0;
 }
+
 int atoi(const char *str){
 	unsigned int i;
 	size_t len = strlen(str);
